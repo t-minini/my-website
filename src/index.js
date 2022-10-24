9541355c658b59267109860481d9c899a9a896ce
Tulio Minini <tulio.mminini@gmail.com>
2022-10-24 16:59:55 -0300

    :wrench: trying to add meta tag image, to when the website is shared. react-snap installed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
 import App from "./App";
+import React from "react";
+import { hydrate, render } from "react-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ 
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
